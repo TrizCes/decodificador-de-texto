b2f5ee1b5cdd024daaa0b25db26e4f9902af8957
Beatriz Ceschini <biaceschini@gmail.com>
2024-01-30 17:11:19 -0300

    copia do texto melhorada

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@
 const text = document.getElementById('text');
 const criptograph = document.getElementById('cript');
 const descript = document.getElementById('descript');
-const newtext = document.getElementById('newtext');
-const copybutton = document.getElementById('copybutton');
+const newText = document.getElementById('newtext');
+const copyButton = document.getElementById('copybutton');
 const ilustration = document.getElementById('graph-ilustration');
 const graphText = document.getElementById('graph-text');
 
 const showNewText = (t) => {
-  newtext.innerHTML = t;
+  newText.innerHTML = t;
+  copyButton.innerText = "Copiar";
 };
 
 criptograph.addEventListener('click', (event) => {
@@ -41,4 +42,14 @@ criptograph.addEventListener('click', (event) => {
 
     graphText.style.display = 'flex';
   }
+});
+
+copyButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const copyText = newText.innerHTML ;
+
+  navigator.clipboard.writeText(copyText);
+
+  copyButton.innerText = "Copiado!"
 });
